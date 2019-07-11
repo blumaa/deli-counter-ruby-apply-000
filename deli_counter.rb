be68737d50e4eb79c0d5c86d4ad877dfbaaabbfe
Aaron Blum <blumaa@gmail.com>
2019-07-11 19:45:36 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,8 +1,11 @@
 katz_deli = []
 
 def line(katz_deli)
-  katz_deli.none?
+  if katz_deli.none?
     puts "The line is currently empty."
+  else
+    katz_deli
+  end
 end
 
 def take_a_number(katz_deli, persons_name)
