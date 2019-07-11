068250c115cf78cea393860e4f68eee26b587bf4
Aaron Blum <blumaa@gmail.com>
2019-07-11 20:27:54 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,7 +5,7 @@ def line(katz_deli)
     puts "The line is currently empty."
   else
       puts "The line is currently: #{katz_deli}" 
-      katz_deli.each_index {|name| puts name, ". " }
+      katz_deli.each_with_index {|name| puts name, ". " }
   end
 end
 
