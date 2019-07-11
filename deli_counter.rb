72ef2398f3b0bcae403747a693f9ad2c0097fe28
Aaron Blum <blumaa@gmail.com>
2019-07-11 20:34:35 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,8 +5,10 @@ def line(katz_deli)
     puts "The line is currently empty."
   else
     the_line = "The line is currently:"
-      katz_deli.each_index {|i| i name, ". " }
-  end
+    # shovel onto the string the index. the element at the index
+      katz_deli.each_index {|i| the_line << "#{i}"
+      puts the_line
+    end
 end
 
 def take_a_number(katz_deli, persons_name)
