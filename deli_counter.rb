53d77c6ddbc8a09da469ab37f55e598a3bf917d5
Aaron Blum <blumaa@gmail.com>
2019-07-11 19:48:45 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,7 @@ def line(katz_deli)
   if katz_deli.none?
     puts "The line is currently empty."
   else
-      puts "The line is currently#{katz_deli}"
+      puts "The line is currently #{katz_deli}"
   end
 end
 
