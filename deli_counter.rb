eb884f08cf918f24998a90743dcce66f95e42176
Aaron Blum <blumaa@gmail.com>
2019-07-11 20:08:55 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,6 +4,8 @@ def line(katz_deli)
   if katz_deli.none?
     puts "The line is currently empty."
   else
+      katz_deli.each_index {|x| puts x, ". #{x}" }
+
       katz_deli.each do |hello|
       puts "The line is currently:  #{hello}"
     end
